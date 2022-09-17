c52adfecabfcab211570c9f88eec8ae9bad5cbc4
Juan Carlos <jcspz0stardan@gmail.com>
2022-09-17 10:19:00 -0400

    Ajustando GetVuelos

--- ms-reserva-infraestructure/src/main/java/com/diplo/infraestructure/msreserva/entityframework/entity/repository/VueloEntityRepository.java
+++ ms-reserva-infraestructure/src/main/java/com/diplo/infraestructure/msreserva/entityframework/entity/repository/VueloEntityRepository.java
@@ -7,14 +7,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VueloEntityRepository
 	extends CrudRepository<VueloEntity, String> {
 	@Query(
-		value = "SELECT * FROM vuelo WHERE origen = ?1 and destino = ?2",
+		value = "SELECT * FROM vuelo WHERE origen = :origen and destino = :destino",
 		nativeQuery = true
 	)
-	List<VueloEntity> GetVuelosByDestino(String origen, String destino);
+	List<VueloEntity> GetVuelosByDestino(
+		@Param("origen") String origen,
+		@Param("destino") String destino
+	);
 }
