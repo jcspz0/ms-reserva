75cb485946104c7f2add2f093759f00689134dce
Juan Carlos <jcspz0stardan@gmail.com>
2022-09-21 00:10:18 -0400

    Ajustando reduccion de disponiblidad en rservaConfiramdaRollback

--- ms-reserva-application/src/main/java/com/diplo/application/msreserva/usecase/consumer/integration/ConsumerIntegrationReservaConfirmadaRollback.java
+++ ms-reserva-application/src/main/java/com/diplo/application/msreserva/usecase/consumer/integration/ConsumerIntegrationReservaConfirmadaRollback.java
@@ -1,16 +1,23 @@
 package com.diplo.application.msreserva.usecase.consumer.integration;
 
+import com.diplo.application.msreserva.dto.reserva.ReservaDTO;
 import com.diplo.application.msreserva.usecase.command.reserva.reservacompletadarollback.ReservaCompletadaRollbackCommand;
 import com.diplo.application.msreserva.usecase.command.reserva.reservacompletadarollback.ReservaCompletadaRollbackHandler;
 import com.diplo.application.msreserva.usecase.command.reserva.vencerreserva.VencerReservaCommand;
 import com.diplo.application.msreserva.usecase.command.reserva.vencerreserva.VencerReservaHandler;
+import com.diplo.application.msreserva.usecase.command.vuelo.reducirdisponibilidad.ReducirDisponibilidadCommand;
+import com.diplo.application.msreserva.usecase.command.vuelo.reducirdisponibilidad.ReducirDisponibilidadHandler;
+import com.diplo.application.msreserva.usecase.query.reserva.getReservaById.GetReservaByIdHandler;
+import com.diplo.application.msreserva.usecase.query.reserva.getReservaById.GetReservaByIdQuery;
 import com.diplo.msreserva.repository.IReservaRepository;
 import com.diplo.msreserva.repository.IUnitOfWork;
+import com.diplo.msreserva.repository.IVueloRepository;
 import com.diplo.sharedkernel.amqp.IAmqpConsumer;
 import com.diplo.sharedkernel.integrationevents.IntegrationDeudaVencida;
 import com.diplo.sharedkernel.integrationevents.IntegrationReservaConfirmadaRollback;
 import com.diplo.sharedkernel.integrationevents.IntegrationReservaCreada;
 import com.diplo.sharedkernel.mediator.IMediator;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -27,6 +34,9 @@ public class ConsumerIntegrationReservaConfirmadaRollback
 
 	@Autowired
 	private IReservaRepository _reservaRepository;
+
+	@Autowired
+	IVueloRepository _vueloRepository;
 
 	@Override
 	@EventListener
@@ -50,6 +60,32 @@ public class ConsumerIntegrationReservaConfirmadaRollback
 				reservaCompletadaRollbackHandler
 			);
 			_mediator.Send(reservaCompletadaRollbackCommand);
+			GetReservaByIdQuery getReservaByIdQuery = new GetReservaByIdQuery(
+				UUID.fromString(message.getReservaId())
+			);
+
+			GetReservaByIdHandler getReservaByIdHandler = new GetReservaByIdHandler(
+				_reservaRepository
+			);
+			_mediator.registrarComando(
+				getReservaByIdQuery,
+				getReservaByIdHandler
+			);
+			ReservaDTO reserva = _mediator.Send(getReservaByIdQuery);
+			ReducirDisponibilidadCommand _reducirDisponibilidadCommand = new ReducirDisponibilidadCommand(
+				reserva.getVueloId().toString(),
+				reserva.getCantidadPasajero() * (-1)
+			);
+
+			ReducirDisponibilidadHandler _reducirDisponibilidadHandler = new ReducirDisponibilidadHandler(
+				_vueloRepository,
+				_unitOfWork
+			);
+			_mediator.registrarComando(
+				_reducirDisponibilidadCommand,
+				_reducirDisponibilidadHandler
+			);
+			_mediator.Send(_reducirDisponibilidadCommand);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
